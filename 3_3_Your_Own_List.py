c2a234e08d1200cb95ec3a8e556a08b2c4f379f8
Pat Callaway <callawayp0705@gmail.com>
2024-06-08 07:21:58 -0400

    3.3 Exercise

---
+++ 3_3_Your_Own_List.py
@@ -0,0 +1,5 @@
+transport = ['bike', 'car', 'bus', 'train', 'plane']
+print(f"I would like to own a {transport[0]}")
+print(f"I would like to own a {transport[1]}")
+print(f"I would like to own a {transport[2]}")
+print(f"I would like to own a {transport[3]}")
