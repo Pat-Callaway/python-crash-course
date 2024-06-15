a3e6c0281c162f714fbb801f3a45409ab30e4c26
Pat Callaway <callawayp0705@gmail.com>
2024-06-15 09:58:45 -0400

    Working with lists

---
+++ Working with Lists.py
@@ -0,0 +1,5 @@
+
+# The below code is an example of a for loop that prints out each name in a list of magicians. where magicians is the list of names and magician is the variable that holds the value of each name in the list.
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
